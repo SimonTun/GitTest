461a5b8e037cbd5e31d6d6ccaf8fb559ece9772d
SimonT <simon@tunstig.se>
2021-12-07 11:47:11 +0100

    Refactor 1

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Change 2");
-        System.out.println("Change 3");
-        System.out.println("Change 4");
-        System.out.println("Change number 5");
-        System.out.println("change number 6...");
+        System.out.println("The program has been refactored");
+
     }
+
 }
